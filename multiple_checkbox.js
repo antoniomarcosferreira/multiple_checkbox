1be89009f6a6cd0d571cb6945e602bb664bbdc92
Antonio Marcos Ferreira <amfcode@gmail.com>
2019-10-10 15:56:12 -0300

    Update multiple_checkbox.js

--- multiple_checkbox.js
+++ multiple_checkbox.js
@@ -1,78 +1,78 @@
-/**
- * multiple_checkbox_ids
- *
- * @author Antonio Marcos Ferreira <amfcode@gmail.com>
- * @version 0.0.1
- * @licence MIT
- * @preserve
- * 
- * 
- * Save selections to localStorage
- * This allows you to browse pagination or filters and select or deselect items
- * 
- * To activate just create a letiable on the page eg:
- * haml
- *  :javascript
- *    let multiple_checkbox_ids = "#{params[:controller]}_#{current_account.id}_ids";
- *
- * Then create a form named 'multiple_checkbox' and in the submit event send the values ​​of the letiable that is in localStorage.
- * In your list create input checkbox with class: 'selectable'
- * At the top of the list create a checkbox with id: 'checkAll'
- */
- 
 $(document).ready(function () {
-  if (multiple_checkbox_ids) {
-    let store_name = multiple_checkbox_ids;
-    let store_name_all = store_name + "_all";
-
-    let ids = localStorage.getItem(store_name);
+  if (typeof multiple_action_ids !== "undefined") {
+    var store_name = multiple_action_ids;
+    var ids = localStorage.getItem(store_name);
+    var checked_count = 0;
     if (ids == undefined) ids = "";
 
     $("#checkAll").change(function () {
-      let a = $(".selectable"), l = $(this).prop("checked");
+      var a = $(".selectable"),
+        l = $(this).prop("checked");
       a.prop("checked", l);
-      localStorage.setItem(store_name_all, l);
-      if (!l) localStorage.setItem(store_name, "");
       set_selected();
     });
 
+
     $(".selectable").change(function () { set_selected(); });
 
-    function set_selected() {
-      let n = $(".selectable");
-      ids = localStorage.getItem(store_name);
+    $("#multiple_action_unchek_all").click(function () {
+      localStorage.setItem(store_name, "");
+      $(".selectable").prop("checked", false);
+      set_selected();
+    });
 
+
+    function set_selected() {
+      var n = $(".selectable");
+      ids = localStorage.getItem(store_name);
       if (ids == undefined) ids = "";
 
+      var local_checked_count = 0;
+
       n.each(function () {
-        let v = "," + this.value;
+        var v = "," + this.value;
         if (this.checked) {
+          local_checked_count += 1;
           if (ids.indexOf(v) < 0) ids += v;
         } else {
           ids = ids.replace(v, "");
         }
       });
 
+      multiple_action_update_values(ids, local_checked_count);
+    }
+
+    if (ids.length > 0) $("#multiple_action").show();
+
+    $("#multiple_action_count").html(ids.split(",").length - 1);
+
+    $(".selectable").each(function () {
+      if (ids.indexOf("," + $(this).val()) >= 0) {
+        checked_count += 1;
+        $(this).prop("checked", "checked");
+      }
+    });
+    
+    multiple_action_update_values(ids, checked_count);
+
+    function multiple_action_update_values(ids, checked_count) {
       localStorage.setItem(store_name, ids);
 
       if (ids.length > 0) {
-        $("#multiple_checkbox").show(200);
+        $("#multiple_action").show(200);
+        $("#multiple_action_count").html(ids.split(",").length - 1);
       } else {
-        $("#multiple_checkbox").hide(200);
+        $("#multiple_action_count").html("");
+        $("#multiple_action").hide(200);
+      }
+
+      $("#multiple_action_itens").val(ids);
+
+      if (checked_count == $(".selectable").length) {
+        $("#checkAll").prop("checked", "checked");
+      } else {
+        $("#checkAll").prop("checked", false);
       }
     }
-
-    if (ids.length > 0) $("#multiple_checkbox").show();
-
-    ids_all = localStorage.getItem(store_name_all);
-    if (ids_all == "true") {
-      $("#checkAll").prop("checked", "checked");
-      $(".selectable").prop("checked", "checked");
-    } else {
-      $(".selectable").each(function () {
-        if (ids.indexOf("," + $(this).val()) >= 0) $(this).prop("checked", "checked");
-      });
-    }
-
   }
 });
